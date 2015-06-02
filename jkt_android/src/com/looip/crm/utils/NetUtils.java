a60301a9b84c3d8a27e9ee559a075613e7f67aff
lxtao369 <lxtao369@126.com>
2015-06-02 11:33:02 +0800

    添加 工具类 注释

--- jkt_android/src/com/looip/crm/utils/NetUtils.java
+++ jkt_android/src/com/looip/crm/utils/NetUtils.java
@@ -11,5 +11,6 @@ public interface NetUtils {
 //	String DOWNLOADAPPURI = "http://1.lxtao369.sinaapp.com/app/padbot.apk";
 //	派宝机器人 360手机助手 下载url
 	String DOWNLOADAPPURI = "http://shouji.360tpcdn.com/150519/f27791cd6d453b982ecee55a42a565b5/cn.inbot.padbotpad_1.apk";
+//	项目列表bean （需要获取到id）
 	String PROJECTITEMBEAN="http://crm.test.looip.com/wap/getprojet?id=";
 }
