8e1b7bf67ec66dcaabdae9fb9d61c2b655eb3595
lxtao369 <lxtao369@126.com>
2015-06-02 11:03:11 +0800

    修改派宝机器人 360手机卫士 下载链接

--- jkt_android/src/com/looip/crm/utils/NetUtils.java
+++ jkt_android/src/com/looip/crm/utils/NetUtils.java
@@ -8,6 +8,8 @@ package com.looip.crm.utils;
  */
 public interface NetUtils {
 
-	String DOWNLOADAPPURI = "http://1.lxtao369.sinaapp.com/app/padbot.apk";
+//	String DOWNLOADAPPURI = "http://1.lxtao369.sinaapp.com/app/padbot.apk";
+//	派宝机器人 360手机助手 下载url
+	String DOWNLOADAPPURI = "http://shouji.360tpcdn.com/150519/f27791cd6d453b982ecee55a42a565b5/cn.inbot.padbotpad_1.apk";
 	String PROJECTITEMBEAN="http://crm.test.looip.com/wap/getprojet?id=";
 }
